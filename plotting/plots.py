0290600126946f697e0424f35c3e67cd9ecb3b57
mcgillies <mcgillies@shaw.ca>
2025-01-29 20:49:29 -0800

    created file for plotting functions

---
+++ plotting/plots.py
@@ -0,0 +1,13 @@
+import seaborn as sns
+from pandas.plotting import scatter_matrix
+
+def plot_top_x_corr(data, num, type):
+
+    correlations = batter_combined.corr()['Fpoints_G'].abs().sort_values(ascending=False)
+    top_cols = correlations.iloc[1:num].index  
+    if type == "scatter":
+        scatter_matrix(batter_combined[top_cols], figsize=(12, 12), diagonal='kde', alpha=0.5)
+    elif type == "correlation":
+        sns.heatmap(batter_combined[top_cols].corr(), annot=True, fmt=".2f", cmap="coolwarm", linewidths=0.5)
+    else:
+        Exception("This is not a valid type")
